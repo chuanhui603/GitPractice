5802ce8fa3aa3f335d1187fdde907a9ae275d5d9
CR2-01\iSpan <chuanhui603@gmail.com>
2023-06-05 14:19:00 +0800

    編寫程式碼

--- PapperScissorRock/Form1.cs
+++ PapperScissorRock/Form1.cs
@@ -12,43 +12,85 @@ namespace PapperScissorRock
 {
     public partial class Form1 : Form
     {
-
-
-
         public Form1()
         {
             InitializeComponent();
         }
 
-
-
-        private void Form1_Load(object sender, EventArgs e)
-        {
-
-        }
     }
 
 
     public enum Gesture
     {
-        Sciccors,Paper,Rock
+        Sciccors, Paper, Rock
+
     }
 
     public enum GameOutCome
     {
-        Win,Lose,Draw
+        Win, Lose, Draw
     }
 
     public class paperScissorRockGame
     {
         public static GameOutCome GameOutCome(Gesture player, Gesture other)
         {
+            GameOutCome result = new GameOutCome();
+            //Draw
+            if (player == Gesture.Paper && other == Gesture.Paper)
+            {
+                result = (GameOutCome)2;
+            }
+            else if (player == Gesture.Rock && other == Gesture.Rock)
+            {
+                result = (GameOutCome)2;
+            }
+            else if (player == Gesture.Sciccors && other == Gesture.Sciccors)
+            {
+                result = (GameOutCome)2;
+            }
+
+            //Win
+            else if (player == Gesture.Paper && other == Gesture.Rock)
+            {
+                result = (GameOutCome)0;
+            }
+            else if (player == Gesture.Rock && other == Gesture.Sciccors)
+            {
+                result = (GameOutCome)0;
+            }
+            else if (player == Gesture.Sciccors && other == Gesture.Paper)
+            {
+                result = (GameOutCome)0;
+            }
+
+            //Lose
+
+            else if (player == Gesture.Paper && other == Gesture.Sciccors)
+            {
+                result = (GameOutCome)1;
+            }
+            else if (player == Gesture.Rock && other == Gesture.Paper)
+            {
+                result = (GameOutCome)1;
+            }
+            else if (player == Gesture.Sciccors && other == Gesture.Rock)
+            {
+                result = (GameOutCome)1;
+            }
+            return result;
+
 
         }
 
         public static Gesture GetGesture()
         {
+            Random random = new Random();
+            var gesturePlayer = random.Next(0, 4);
+            var gestureOther = random.Next(0, 4);
 
+            GameOutCome((Gesture)gesturePlayer, (Gesture)gestureOther);
+            
         }
     }
 }
