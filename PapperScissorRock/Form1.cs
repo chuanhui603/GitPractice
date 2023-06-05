1c9b0fc43345d58b883162152091bc22d93ad36e
Hui <chuanhui603@gmail.com>
2023-06-05 08:05:21 +0800

    加入剪刀石頭布

---
+++ PapperScissorRock/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PapperScissorRock
+{
+    public partial class Form1 : Form
+    {
+
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+
+
+    public enum Gesture
+    {
+        Sciccors,Paper,Rock
+    }
+
+    public enum GameOutCome
+    {
+        Win,Lose,Draw
+    }
+
+    public class paperScissorRockGame
+    {
+        public static GameOutCome GameOutCome(Gesture player, Gesture other)
+        {
+
+        }
+
+        public static Gesture GetGesture()
+        {
+
+        }
+    }
+}
